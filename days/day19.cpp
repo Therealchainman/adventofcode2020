a296bb9ef89199d04251623c8968e78da46ead6c
Todd Chaney <toddey65@gmail.com>
2021-01-04 00:18:34 -0600

    Completed all 25 days of Advent of Code woot

--- days/day19.cpp
+++ days/day19.cpp
@@ -11,21 +11,6 @@ string trim(string s) {
     return ans;
 }
 
-template<typename T>
-bool isMatch(T pattern, string text) {
-    if (text.size()!=pattern.size()) {
-        return false;
-    }
-    int n = text.size();
-    int i = 0;
-    for (i = 0;i<n;i++) {
-        if (pattern[i]!=string(1,text[i])) {
-            return false;
-        }
-    }
-    return i==n;
-}
-
 bool isNumber(const string &s) {
   return !s.empty() && all_of(s.begin(), s.end(), ::isdigit);
 }
@@ -53,6 +38,7 @@ struct rule {
         if (foundChar<sizeMax) {
             isChar = true;
             character = trim(line.substr(foundChar));
+            R1.push_back(character);
             return;
         }
         size_t found = line.find(" |");
@@ -77,122 +63,80 @@ struct rule {
 
 struct message {
     vector<string> texts;
-    map<int, rule*> rules;
+    unordered_map<int, rule*> rules;
 };
 
-// not using this one. 
-template<typename T>
-vector<T> create_copy(vector<T> const &vec) {
-    vector<T> v(vec.size());
-    copy(vec.begin(),vec.end(),v.begin());
-    return v;
-}
-
-bool noNumbers(vector<string> vec) {
-    for (string v : vec) {
-        if (isNumber(v)) {
-            return false;
-        }
-    }
-    return true;
-}
-
-void printString(vector<string> t) {
-    string str = "";
-    for (string x : t) {
-        str+=x;
-    }
-    
+bool isTerminal(rule* rule) {
+    return rule->isChar;
 }
 
 int solve(message m) {
     int ans = 0;
-    for (string text : m.texts) {
-        set<vector<string>> vis;
-        cout<<text<<endl;
-        vector<vector<string>> patterns;
-        bool onlyChars = false;
-        vector<string> start = {"0"};
-        patterns.push_back(start);
-        while (!onlyChars && !patterns.empty()) {
-            onlyChars = true;
-            vector<vector<string>> vec;
-            int k = 0;
-            int counter = 0;
-            while (k<patterns.size()) {
-                vector<string> pat = patterns[k];
-                if (vis.count(pat)) {
-                    patterns.erase(patterns.begin()+k);
+    for (string msg : m.texts) {
+        vector<vector<string>> patterns(1, vector<string>(1,"0"));
+        for (int i = 0;i<msg.size();i++) {
+            vector<vector<string>> nextPatterns;
+            for (int k = 0;k<patterns.size();k++) {
+                vector<string> pattern = patterns[k];
+                if (pattern.size()<=i) {
                     continue;
                 }
-                vis.insert(pat);
-                if (pat.size()>text.size()) {
-                    patterns.erase(patterns.begin()+k);
-                    continue;
-                }
-                if (pat.size()==text.size() && noNumbers(pat)) {
-                    ans+=isMatch(pat,text);
-                    patterns.erase(patterns.begin()+k);
-                    continue;
-                }
-                k++;
-                vector<vector<string>> patterns2;
-                vector<string> pat2;
-                patterns2.push_back(pat2);
-                for (string s : pat) {
-                    if (patterns2.empty()) {
+                while (isNumber(pattern[i])) {
+                    if (pattern.size()>msg.size()) {
                         break;
                     }
-                    if (isNumber(s)) {
-                        onlyChars = false;
-                        int num = stoi(s);
-                        rule* r = m.rules[num];
-    
-                        if (r->isChar) {
-                            int j = 0;
-                            for (int j = 0;j<patterns2.size();j++) {
-                                patterns2[j].push_back(r->character);
+                    int rKey = stoi(pattern[i]);
+                    rule* cRule = m.rules[rKey];
+                    if (isTerminal(cRule)) {
+                        pattern[i]=cRule->character;
+                        continue;
+                    }
+                    // simple rule
+                    if (cRule->subR2.empty()) {
+                        for (int j = 0;j<cRule->R1.size();j++) {
+                            string nextRuleId = cRule->R1[j];
+                            if (j==0) {
+                                pattern[i]=nextRuleId;
+                                continue;
                             }
-                            continue;
-                        }
-                        if (r->numSubs==1) {
-                            for (string s1 : r->R1) {
-                                for (int i = 0;i<patterns2.size();i++) {
-                                    patterns2[i].push_back(s1);
-                                }
-                            }
-                        } else if (r->numSubs==2) {
-                            vector<vector<string>> cpatterns = patterns2;
-                            for (string s1 : r->R1) {
-                                for (int i = 0;i<patterns2.size();i++) {
-                                    patterns2[i].push_back(s1);
-                                }
-                            }
-                            for (string s2 : r->R2) {
-                                counter++;
-                                for (int i =0;i<cpatterns.size();i++) {
-                                    cpatterns[i].push_back(s2);
-                                }
-                            }
-                            patterns2.insert(patterns2.end(),cpatterns.begin(),cpatterns.end());
+                            pattern.insert(pattern.begin()+i+j,nextRuleId);
                         }
                     } else {
-                        for (int i = 0;i<patterns2.size();i++) {
-                            patterns2[i].push_back(s);
+                        vector<string> cPattern = pattern;
+                        for (int j = 0;j<cRule->R1.size();j++) {
+                            string nextRuleId = cRule->R1[j];
+                            if (j==0) {
+                                pattern[i]=nextRuleId;
+                                continue;
+                            }
+                            pattern.insert(pattern.begin()+i+j,nextRuleId);
                         }
+                        for (int j = 0;j<cRule->R2.size();j++) {
+                            string nextRuleId = cRule->R2[j];
+                            if (j==0) {
+                                cPattern[i]=nextRuleId;
+                                continue;
+                            }
+                            cPattern.insert(cPattern.begin()+j+i,nextRuleId);
+                        }
+                        patterns.push_back(cPattern);
                     }
                 }
-                vec.insert(vec.end(),patterns2.begin(),patterns2.end());
+                string s;
+                s.push_back(msg[i]);
+                if (pattern[i]==s && pattern.size()<=msg.size()) {
+                    nextPatterns.push_back(pattern);
+                }
             }
-            cout<<counter<<endl;
-            patterns = vec;
+            patterns = nextPatterns;
         }
+        ans+=patterns.size();
     }
     return ans;
 }
 
 int main() {
-    freopen("inputs/small2.txt","r",stdin);
+    freopen("inputs/big.txt","r",stdin);
     string input;
     message m;
     while (getline(cin,input)) {
@@ -202,7 +146,6 @@ int main() {
         rule* r = new rule(input);
         m.rules[r->name] = r;
     }
-    cout<<m.rules[0]<<endl;
     while (getline(cin,input)) {
         m.texts.push_back(input);
     }
